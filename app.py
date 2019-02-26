41d06704c2cdb87f8aa739dc5a3ac23e53aa4984
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-26 11:55:13 -0800

    Added bcrypt and start app.py.

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template, session, bcrypt
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes-app'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
+connect_db(app)
+
