5d544e398e32aafe1d1c0405f0ade5a5a8a62cb1
Sandy <sangzicao@gmail.com>
2019-02-26 14:59:30 -0800

    made password all asterisks

--- forms.py
+++ forms.py
@@ -1,14 +1,14 @@
 """Forms for Feedback app."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextField
+from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
 
 class RegisterForm(FlaskForm):
     """Form for registering a user."""
 
     username = StringField("Username", validators=[InputRequired(), Length(max=10)])
-    password = TextField("Password", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
     email = StringField("Email", validators=[InputRequired(), Length(max=50)])
     first_name = StringField("First Name", validators=[InputRequired(), Length(max=30)])
     last_name = StringField("Last Name", validators=[InputRequired(), Length(max=30)])
