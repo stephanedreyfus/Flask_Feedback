bdaf535eda7dad3b9c3f273aaaf05e63e0e7d47f
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-26 15:33:25 -0800

    Linter fixes.

--- forms.py
+++ forms.py
@@ -4,11 +4,24 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
 
+
 class RegisterForm(FlaskForm):
     """Form for registering a user."""
 
-    username = StringField("Username", validators=[InputRequired(), Length(max=10)])
+    username = StringField("Username", validators=[InputRequired(),
+                                                   Length(max=10)])
     password = PasswordField("Password", validators=[InputRequired()])
     email = StringField("Email", validators=[InputRequired(), Length(max=50)])
-    first_name = StringField("First Name", validators=[InputRequired(), Length(max=30)])
-    last_name = StringField("Last Name", validators=[InputRequired(), Length(max=30)])
+    first_name = StringField("First Name", validators=[InputRequired(),
+                                                       Length(max=30)])
+    last_name = StringField("Last Name", validators=[InputRequired(),
+                                                     Length(max=30)])
+
+
+class LoginForm(FlaskForm):
+    """ Form for login and user authentication. """
+
+    username = StringField("Username",
+                           validators=[InputRequired(),
+                                       Length(max=10)])
+    password = PasswordField("Password", validators=[InputRequired()])
